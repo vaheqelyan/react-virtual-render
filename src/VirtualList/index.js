6c7d370f010931eb0cb27140a6b7d483dc0606d2
Vahe <martinkenner@mail.com>
2018-05-22 12:44:41 +0400

    -4,0 +5

--- src/VirtualList/index.js
+++ src/VirtualList/index.js
@@ -2,6 +2,7 @@ import React from "react";
 
 export default class VirtualList extends React.Component {
   handleScroll = () => {};
+  resize = () => {};
   componentDidMount() {}
   componentDidUpdate() {
     this.resize();
