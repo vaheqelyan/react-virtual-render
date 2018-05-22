7a8ff35550f7b711e6bbdbcf1bc24b6ed204f75e
Vahe <martinkenner@mail.com>
2018-05-22 13:37:05 +0400

    Fixed:: React does not recognize the prop on a DOM element

--- src/VirtualList/index.js
+++ src/VirtualList/index.js
@@ -75,7 +75,7 @@ export default class VirtualList extends React.Component {
     const { selection, start } = state;
 
     return (
-      <div ref={setBase} onScroll={handleScroll} {...props}>
+      <div ref={setBase} onScroll={handleScroll}>
         <div
           style={{
             position: "relative",
