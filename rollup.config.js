453b0aee6d58f6c41c5e26123e3a7b9b7ef312bf
Vahe <martinkenner@mail.com>
2018-05-22 13:03:28 +0400

    Configure

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+// rollup.config.js
+import resolve from "rollup-plugin-node-resolve";
+import babel from "rollup-plugin-babel";
+import commonjs from "rollup-plugin-commonjs";
+import alias from "rollup-plugin-alias";
+export default [
+  {
+    input: "./src/VirtualList/index.js",
+    name: "RVRVirtualList",
+    output: {
+      file: "build/rvr-virtual-list.js",
+      format: "umd"
+    },
+    plugins: [
+      babel({ exclude: "node_modules/**" }),
+      resolve({
+        jsnext: true,
+        main: true
+      }),
+      commonjs({
+        include: "node_modules/**"
+      }),
+      alias({
+        react: "./node_modules/react/umd/react.development.js"
+      })
+    ],
+    external: ["react"],
+    globals: {
+      react: "React"
+    }
+  }
+];
