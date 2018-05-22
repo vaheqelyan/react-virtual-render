55f21236efe0bcb5dc6ed02db951b67525074183
Vahe <martinkenner@mail.com>
2018-05-22 12:43:30 +0400

    -4 +4,11

--- src/VirtualList/index.js
+++ src/VirtualList/index.js
@@ -1,5 +1,15 @@
 import React from "react";
 
 export default class VirtualList extends React.Component {
-  render() {}
+  render() {
+    return (
+      <div onScroll={this.handleScroll} {...props}>
+        <div style={`${STYLE_INNER} height:${data * rowHeight}px;`}>
+          <div style={`${STYLE_CONTENT} top:${start * rowHeight}px;`}>
+            {renderRow(start, end)}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
