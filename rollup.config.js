d72269bb446f73b919a392e8e0098a1e7bb8a29a
Vahe <martinkenner@mail.com>
2018-05-22 14:23:08 +0400

    -6,0 +7,26

--- rollup.config.js
+++ rollup.config.js
@@ -28,5 +28,31 @@ export default [
     globals: {
       react: "React"
     }
+  },
+  /* HorizontalList */
+  {
+    input: "./src/VirtualList/index.js",
+    name: "RVRVirtualList",
+    output: {
+      file: "build/rvr-virtual-list.js",
+      format: "umd"
+    },
+    plugins: [
+      babel({ exclude: "node_modules/**" }),
+      resolve({
+        jsnext: true,
+        main: true
+      }),
+      commonjs({
+        include: "node_modules/**"
+      }),
+      alias({
+        react: "./node_modules/react/umd/react.development.js"
+      })
+    ],
+    external: ["react"],
+    globals: {
+      react: "React"
+    }
   }
 ];
